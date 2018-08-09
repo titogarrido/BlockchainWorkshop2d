29268fe71b62515c0333acd5510605305c5d5f06
Tito Garrido Ogando <titogarrido@gmail.com>
2018-08-09 02:18:53 -0300

    first drop

---
+++ README.md
@@ -0,0 +1,30 @@
+# Getting started with Fabric and Composer
+
+Esse documento descreve o Meetup com hands-on. Os arquivos necessários para a execução se encontram neste repositório.
+
+
+
+## Pre-requisitos
+
+- [x] Criar sua conta na Linux Community Cloud (https://developer.ibm.com/linuxone/request-a-trial/)
+- [x] Noção Básica de Linux
+- [x] Noção Básica de Programação
+- [x] Cliente SSH
+
+
+
+## Crie sua instancia
+
+Para nosso exercício iremos utilizar uma instancia SuSE 12 SP3.
+
+![](https://raw.githubusercontent.com/titogarrido/meetup-hands-on/master/img/flavor.png)
+
+Depois de criado o seu servidor terá um IP válido. O usuário para acesso é *linux1* e utilizará a chave que você especificou quando criou a instância.
+
+![](https://raw.githubusercontent.com/titogarrido/meetup-hands-on/master/img/server.png)
+
+
+
+Utilize o cliente SSH de sua preferência com sua chave e acesse o servidor.
+
+![](https://raw.githubusercontent.com/titogarrido/meetup-hands-on/master/img/ssh.png)
---
+++ suse-prep.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+sudo zypper install -y --type pattern Basis-Devel
+sudo zypper install -y libopenssl-devel
+
+# Execute nvm installation script
+echo "# Executing nvm installation script"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# Set up nvm environment without restarting the shell
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
+
+# Install node
+echo "# Installing nodeJS"
+nvm install --lts
+
+# Install the latest version of npm
+echo "# Installing npm"
+npm install npm@latest -g
+
+# Add docker privileges
+
+sudo usermod -aG docker $(whoami)
+
+# Docker compose
+
+sudo easy_install pip
+
+sudo pip install docker-compose==1.13
+
+# Installing Golang
+cd /tmp
+wget --no-check-certificate https://storage.googleapis.com/golang/go1.9.3.linux-s390x.tar.gz
+tar -xvf go1.9.3.linux-s390x.tar.gz
+sudo mv -iv go /opt
+cd ~/
+cp -ipv .bashrc .bashrc_orig
+echo export GOPATH=/home/$USER/git >> .bashrc
+echo export GOROOT=/opt/go >> .bashrc
+echo export PATH=/opt/go/bin:/home/$USER/bin:\$PATH >> .bashrc
+
+# Installing composer
+npm install -g composer-cli@0.19
+npm install -g composer-rest-server@0.19
+npm install -g generator-hyperledger-composer@0.19
+npm install -g yo
+npm install -g composer-playground@0.19
+
+# Add firewall rules
+sudo iptables -i eth0 -I INPUT -p tcp --dport 3000 -j ACCEPT
+sudo iptables -i eth0 -I INPUT -p tcp --dport 4200 -j ACCEPT
+sudo iptables -i eth0 -I INPUT -p tcp --dport 8080 -j ACCEPT
+sudo iptables-save > ~/iptables.save
+
+# Fetch some utils
+cd ~/
+git clone https://github.com/hyperledger/composer-sample-applications.git
+git clone https://github.com/hyperledger/composer-tools.git
+
+
+# Print installation details for user
+echo ''
+echo 'Installation completed, versions installed are:'
+echo ''
+echo -n 'Node:           '
+node --version
+echo -n 'npm:            '
+npm --version
+echo -n 'Docker:         '
+docker --version
+echo -n 'Docker Compose: '
+docker-compose --version
+echo -n 'Python:         '
+python -V
+echo -n 'Hyperledger Composer:         '
+composer --version
